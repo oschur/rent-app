82d582f71b0c8f33dbeb9901e2ce7ba6bb187ea1
oschur <genda5656@gmail.com>
2025-12-25 03:08:48 +0500

    feat: add user service interface

---
+++ internal/domain/user/service.go
@@ -0,0 +1,11 @@
+package user
+
+type Service interface {
+	CreateUser(email, password, firstname, lastname string, isLandlord, isAdmin bool) (*User, error)
+	GetUserByID(id int) (*User, error)
+	GetUserByEmail(email string) (*User, error)
+	GetAllUsers() ([]*User, error)
+	UpdateUser(id int, email, firstname, lastname *string, isLandlord, isAdmin *bool) (*User, error)
+	DeleteUser(id int) error
+	ResetPassword(id int, password string) error
+}
